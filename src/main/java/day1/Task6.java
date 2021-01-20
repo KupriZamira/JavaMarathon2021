b5759c0b781de56726b0f93de7fdd1962f045db2
Zamira Kupriyanova <kupriyanova.zamira@gmail.com>
2021-01-20 21:58:02 +0300

    Add Day1 solutions

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 5;
+        for(int a = 1; a <= 10; a++){
+            int b = a * k;
+            System.out.println(a + " x " + k + " = " + b);
+        }
     }
 }
