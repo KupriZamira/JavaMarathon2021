144785b7239f9ece698b1ade6ccba18e98b20818
Zamira Kupriyanova <kupriyanova.zamira@gmail.com>
2021-01-26 17:10:41 +0300

    Add Day2 solutions (task4)

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -7,16 +7,17 @@ public class Task4 {
         System.out.println("Введите число x");
         Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
+        double y;
         if (x >= 5){
-            double y = (Math.pow(x, 2) - 10) / (x + 7);
-            System.out.println(y);
+            y = (Math.pow(x, 2) - 10) / (x + 7);
         }else if(- 3 < x && x < 5){
-            double y = (x + 3) * (Math.pow(x, 2) - 2);
-            System.out.println(y);
+            y = (x + 3) * (Math.pow(x, 2) - 2);
         }else {
-            double i = 420;
-            System.out.println(i);
+            y = 420;
         }
+        System.out.println(y);
+
+
 
     }
 
