3029b6dc8c8f542f70d41d6d9b213b9199dba3c9
Zamira Kupriyanova <kupriyanova.zamira@gmail.com>
2021-01-27 17:15:51 +0300

    Add Day3 solutions

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,45 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        System.out.println("Введите город");
+        Scanner scanner = new Scanner(System.in);
+        String stop = "Stop";
+        do {
+            String city = scanner.nextLine();
+            if (stop.equals(city)){
+                break;
+            }
+            switch (city) {
+                case "Владивосток":
+                case "Москва":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин ":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн ":
+                    System.out.println("Германия");
+                    break;
+
+                default:
+                    System.out.println("Неизвестная страна");
+                    break;
+            }
+        } while (true);
 
     }
 }
+
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println("Введите числа делитель и делимое");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0){
+                System.out.println("На ноль делить нельзя");
+                break;
+            }
+            double c = a  / b;
+            System.out.println(c);
+            continue;
+        }
 
     }
 }
