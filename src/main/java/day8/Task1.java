a8fcf0988c10d7c7599247fcacb3231c20ac3b55
Zamira Kupriyanova <kupriyanova.zamira@gmail.com>
2021-03-01 19:19:46 +0300

    Solutions Day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,25 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        String str = "";
+        for(int i = 0; i <= 2000; i++) {
+            str = str + i + " ";
+        }
+        System.out.println(str);
+
+        long startTime = System.nanoTime();
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.println(estimatedTime);
+
+
+        StringBuilder string1 = new StringBuilder();
+        for (int i = 0; i <= 2000; i++){
+            string1.append(i + " ");
+        }
+        System.out.println(string1.toString());
+
+        long estimatedTime1 = System.nanoTime() - startTime;
+        System.out.println(estimatedTime1);
     }
 }
+
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,7 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane(575757, "Boeing", 1990, 340000, 45000);
+        System.out.println(airplane);
     }
 }
